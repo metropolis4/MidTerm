b92949b31c7083257e666aafff7e8d254f36c41b
Matt Rich <metropolis_4@hotmail.com>
2015-02-13 01:46:41 -0700

    Final for presentation

--- scripts/main.js
+++ scripts/main.js
@@ -52,6 +52,53 @@ ScheduledDate.dates = [];
 
 // Helper Functions
 // 
+var createNewScheduleDate = function(date){
+    dateArray = date.split('-');
+                var schDateYear = dateArray[0];
+                var schDateDay = dateArray[2];
+                Number(schDateDay) < 10 ? schDateDay = schDateDay.charAt(1) : schDateDay = schDateDay;
+                var schDateMonth = dateArray[1];
+                switch(schDateMonth) {
+                    case '01':
+                        schDateMonth = 'January';
+                        break;
+                    case '02':
+                        schDateMonth = 'February';
+                        break;
+                    case '03':
+                        schDateMonth = 'March';
+                        break;
+                    case '04':
+                        schDateMonth = 'April';
+                        break;
+                    case '05':
+                        schDateMonth = 'May';
+                        break;
+                    case '06':
+                        schDateMonth = 'June';
+                        break;
+                    case '07':
+                        schDateMonth = 'July';
+                        break;
+                    case '08':
+                        schDateMonth = 'August';
+                        break;
+                    case '09':
+                        schDateMonth = 'September';
+                        break;
+                    case '10':
+                        schDateMonth = 'October';
+                        break;
+                    case '11':
+                        schDateMonth = 'November';
+                        break;
+                    default:
+                        schDateMonth = 'December';
+                        break;
+                }
+    return [schDateMonth, schDateDay, schDateYear];
+};
+
 var populateNewDates = function(){
     return  _.map(ScheduledDate.dates, function(val){
         var $el = $('.scheduled-date').first().clone();
@@ -93,6 +140,7 @@ var populateVolunteersNewSchedule = function(){
             var list = $el.find('.vol-list-item').first().clone();
             list.removeClass('hiding');
             list.find('.name').text(val.fullName);
+            list.find('input').attr('value', val.fullName);
             return $el.find('ul').append(list);
         });
         return $el;
@@ -134,31 +182,31 @@ var findSub = function(age){
 var ageArray = ['infants', '12-18month', '18-24month', '2-3yrs', '3-4yrs', '4-5yrs'];
 // Create Demo Volunteer List
 // Infants, 12-18 month, 18-24month, 2-3yrs, 3-4yrs, 4-5yrs
-var matilda = new Volunteer('Matilda', 'Rich', '123-456-7890', 'Matilda@email.com', ['infants'], 'no', 'yes');
-var court = new Volunteer('Court', 'Rich', '123-456-7890', 'court@email.com', ['infants'], 'yes', 'yes');
-var leia = new Volunteer('Leia', 'Organa', '123-456-7890', 'Leia@email.com', ['12-18month'], 'yes', 'yes');
-var maggie = new Volunteer('Maggie', 'Smith', '123-456-7890', 'Maggie@email.com', ['12-18month'], 'no', 'yes');
-var lisa = new Volunteer('Lisa', 'Simpson', '123-456-7890', 'lisa@email.com', ['18-24month'], 'no', 'yes');
-var peggy = new Volunteer('Peggy', 'Sue', '123-456-7890', 'peggy@email.com', ['18-24month'], 'yes', 'yes');
-var clarabelle = new Volunteer('Clarabelle', 'Cow', '123-456-7890', 'Clarabelle@email.com', ['2-3yrs'], 'no', 'yes');
-var Grissella = new Volunteer('Grissella', 'Grimm', '123-456-7890', 'Grissella@email.com', ['2-3yrs'], 'yes', 'yes');
-var eleanor = new Volunteer('Eleanor', 'Rigby', '123-456-7890', 'Eleanor@email.com', ['3-4yrs'], 'no', 'yes');
-var lizzy = new Volunteer('Lizzy', 'Bennet', '123-456-7890', 'Lizzy@email.com', ['3-4yrs'], 'yes', 'yes');
-var maude = new Volunteer('Maude', 'Clark', '123-456-7890', 'Maude@email.com', ['4-5yrs'], 'no', 'yes');
-var alice = new Volunteer('Alice', 'Carrol', '123-456-7890', 'Alice@email.com', ['4-5yrs'], 'yes', 'yes');
+new Volunteer('Arabella', 'Fig', '123-456-7890', 'Matilda@email.com', ['infants'], 'no', 'yes');
+new Volunteer('Court', 'Rich', '123-456-7890', 'court@email.com', ['infants'], 'yes', 'yes');
+new Volunteer('Perenelle', 'Flamel', '123-456-7890', 'Leia@email.com', ['12-18month'], 'yes', 'yes');
+new Volunteer('Maggie', 'Smith', '123-456-7890', 'Maggie@email.com', ['12-18month'], 'no', 'yes');
+new Volunteer('Hepzibah', 'Smith', '123-456-7890', 'lisa@email.com', ['18-24month'], 'no', 'yes');
+new Volunteer('Emmeline', 'Vance', '123-456-7890', 'peggy@email.com', ['18-24month'], 'yes', 'yes');
+new Volunteer('Romilda', 'Vane', '123-456-7890', 'Clarabelle@email.com', ['2-3yrs'], 'no', 'yes');
+new Volunteer('Andromeda', 'Tonks', '123-456-7890', 'Grissella@email.com', ['2-3yrs'], 'yes', 'yes');
+new Volunteer('Eleanor', 'Rigby', '123-456-7890', 'Eleanor@email.com', ['3-4yrs'], 'no', 'yes');
+new Volunteer('Lizzy', 'Bennet', '123-456-7890', 'Lizzy@email.com', ['3-4yrs'], 'yes', 'yes');
+new Volunteer('Mafalda', 'Hopkirk', '123-456-7890', 'Maude@email.com', ['4-5yrs'], 'no', 'yes');
+new Volunteer('Alice', 'Carrol', '123-456-7890', 'Alice@email.com', ['4-5yrs'], 'yes', 'yes');
 
 // Create Demo Substitute List
-var rita = new Sub('Rita', 'Maid', '123-456-7890', 'rita@email.com', ['infants','12-18month'], 'no', 'yes');
-var miss = new Sub('Miss', 'Molly', '123-456-7890', 'miss@email.com', ['4-5yrs', '3-4yrs', '2-3yrs'], 'no', 'yes');
-var rosey = new Sub('Rosey', 'Cotton', '123-456-7890', 'rosey@mail.com', ['18-24month', 'infants','12-18month'], 'no', 'yes');
-var bathilda = new Sub('Batilda', 'Bagshot', '123-456-7890', 'bathilda@email.com', ['12-18month', '4-5yrs'], 'no', 'yes');
+new Sub('Alicia', 'Spinnet', '123-456-7890', 'rita@email.com', ['infants','12-18month'], 'no', 'yes');
+new Sub('Miss', 'Molly', '123-456-7890', 'miss@email.com', ['4-5yrs', '3-4yrs', '2-3yrs'], 'no', 'yes');
+new Sub('Rosey', 'Cotton', '123-456-7890', 'rosey@mail.com', ['18-24month', 'infants','12-18month'], 'no', 'yes');
+new Sub('Batilda', 'Bagshot', '123-456-7890', 'bathilda@email.com', ['12-18month', '4-5yrs'], 'no', 'yes');
 
 // Create Demo Dates
 // 
-var march5 = new ScheduledDate('March', 5, 2015, 'Court Rich', 'Maggie Smith', 'Peggy Sue', 'Grissella Grimm', 'Eleanor Rigby', 'Alice Carrol');
-var march12 = new ScheduledDate('March', 12, 2015, 'Court Rich', 'Maggie Smith', 'Peggy Sue', 'Grissella Grimm', 'Eleanor Rigby', 'Alice Carrol');
-var march19 = new ScheduledDate('March', 19, 2015, 'Court Rich', 'Maggie Smith', 'Peggy Sue', 'Grissella Grimm', 'Eleanor Rigby', 'Alice Carrol');
-var march26 = new ScheduledDate('March', 26, 2015, 'Court Rich', 'Maggie Smith', 'Peggy Sue', 'Grissella Grimm', 'Eleanor Rigby', 'Alice Carrol');
+new ScheduledDate('March', 5, 2015, 'Court Rich', 'Maggie Smith', 'Peggy Sue', 'Grissella Grimm', 'Eleanor Rigby', 'Alice Carrol');
+new ScheduledDate('March', 12, 2015, 'Court Rich', 'Maggie Smith', 'Peggy Sue', 'Grissella Grimm', 'Eleanor Rigby', 'Alice Carrol');
+new ScheduledDate('March', 19, 2015, 'Court Rich', 'Maggie Smith', 'Peggy Sue', 'Grissella Grimm', 'Eleanor Rigby', 'Alice Carrol');
+new ScheduledDate('March', 26, 2015, 'Court Rich', 'Maggie Smith', 'Peggy Sue', 'Grissella Grimm', 'Eleanor Rigby', 'Alice Carrol');
 
 
 $(document).on('ready', function() {
@@ -183,6 +231,24 @@ $(document).on('ready', function() {
         $('#pick-volunteers').append(populateVolunteersNewSchedule());
         $('.ui.checkbox').checkbox();
         $('#create-schedule').modal('setting', 'transition', 'horizontal flip').modal('show');
+
+        $('#submit-new-sch').one('click', function(){
+            var newSchDate = $('#add-new-date').val();
+            var newSchMonth = createNewScheduleDate(newSchDate)[0];
+            var newSchDay = createNewScheduleDate(newSchDate)[1];
+            var newSchYear = createNewScheduleDate(newSchDate)[2];
+            var selectedVolunteers = [];
+            $('#pick-volunteers').find('.checked').find('.name').each(function(){
+                selectedVolunteers.push($(this).text());
+            });
+            var volunteerArray = _.map(selectedVolunteers, function(val){
+                var innerVal = val;
+                return _.map(Volunteer.volunteers, function(volunteer){
+                    if(volunteer.fullName === innerVal) {return [volunteer.ages, volunteer.fullName]}
+                });
+            });
+            // new ScheduledDate(newSchMonth, newSchDay, newSchYear)
+        });
     });
 
     $('#main-menu').on('click', '#allV', function(){
@@ -199,48 +265,38 @@ $(document).on('ready', function() {
 
     $('#main-menu').on('click', '#newV', function(){
         $('.age-classes-vol').empty();
-        $('.ui.checkbox').checkbox('uncheck');
         $('.age-classes-vol').append(populateAges());
         $('.ui.checkbox').checkbox();
         $('#new-volunteer').modal('setting', 'transition', 'horizontal flip').modal('show');
         $('#submit-new-vol').one('click', function(){
             var fName = $("#first-name").val();
-                $('#first-name').val('');
             var lName = $('#last-name').val();
-                $('#last-name').val('');
             var phNum = $('#phone').val();
-                $('#phone').val('');
             var eMail = $('#email').val();
-                $('#email').val('');
             var isLead = $('#lead').checkbox('is checked');
                 if(isLead) {isLead = "yes"} else{isLead = "no"};
             var bckCheck = $('#background-check').checkbox('is checked');
                 if(bckCheck){bckCheck = 'yes'} else{bckCheck = 'no'};
-                console.log(isLead);
             var ageRange = $('.age-classes-vol').find('.ui.checkbox').checkbox('is checked');
             var newVolAges = _.map(_.zip(ageRange, ageArray), function(val){
                 if(val[0] === true) { return val[1] }
             });
             newVolAges = _.compact(newVolAges);
             new Volunteer(fName, lName, phNum, eMail, newVolAges, isLead, bckCheck);
+            $('.ui.form').form('clear');
         });
     });
 
     $('#main-menu').on('click', '#newS', function(){
         $('.age-classes-sub').empty();
-        $('.ui.checkbox').checkbox('uncheck');
         $('.age-classes-sub').append(populateAges());
         $('.ui.checkbox').checkbox();
         $('#new-substitute').modal('setting', 'transition', 'horizontal flip').modal('show');
         $('#submit-new-sub').one('click', function(){
             var subFirstName = $('.first-name').val();
-                $('.first-name').val('');
             var subLastName = $('.last-name').val();
-                $('.last-name').val('');
             var subPhone = $('.phone').val();
-                $('.phone').val('');
             var subEmail = $('.email').val();
-                $('.email').val('');
             var subIsLead = $('.is-lead').checkbox('is checked');
                 if(subIsLead) {subIsLead = 'yes'} else{subIsLead = 'no'};
             var subBckCheck = $('.is-bckcheck').checkbox('is checked');
@@ -251,19 +307,13 @@ $(document).on('ready', function() {
             });
             newSubAges = _.compact(newSubAges);
             new Sub(subFirstName, subLastName, subPhone, subEmail, newSubAges, subIsLead, subBckCheck);
+            $('.ui.form').form('clear');
         });
     });
 
     $('.month-list').on('click', '.item', function(){
         var thisMonth = $(this).text();
         $('#month').text(thisMonth);
-        console.log('month: ' + month + ' thisMonth: ' + thisMonth);
-        if(month === thisMonth) {
-            $('.scheduled-date').removeClass('hiding');
-        }
-        else{
-            // $('.scheduled-date').addClass('hiding');
-        }
     });
 
     $('#schedule').on('click', '.sub-needed', function(){
